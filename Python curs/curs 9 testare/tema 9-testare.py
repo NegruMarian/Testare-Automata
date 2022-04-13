e6712021730a8edb5f720a09cbc8556582b52590
Marian Negru <Negru Marian Alexandru>
2022-04-13 17:47:08 +0300

    tema9 act

--- Python curs/curs 9 testare/tema 9-testare.py
+++ Python curs/curs 9 testare/tema 9-testare.py
@@ -16,78 +16,28 @@
 # Class name*
 # Css (1 dupa id, 1 dupa clasa, 1 dupa atribut=valoare_partiala)
 
-# from time import sleep
-# from selenium import webdriver
-# from selenium.webdriver.chrome.service import Service
-# from webdriver_manager.chrome import ChromeDriverManager
-# from selenium.webdriver.common.by import By
-
-#1 find element by class_name
-# s = Service(ChromeDriverManager().install())
-# chrome = webdriver.Chrome(service=s)
-# chrome.maximize_window()
-# chrome.get('http://automationpractice.com/index.php')
-#
-# sleep(3)
-# first_name = chrome.find_elements(By.CLASS_NAME, 'sf-with-ul') #trebuie dor sa mai interactionez cu un element
-# stocare_elemente = chrome.find_elements(By.CLASS_NAME, 'sf-with-ul')
-# print(len(stocare_elemente))
-# sleep(3)
-# chrome.quit()
-#
-# #2. find element by ID
-# from time import sleep
-# from selenium import webdriver
-# from selenium.webdriver.chrome.service import Service
-# from webdriver_manager.chrome import ChromeDriverManager
-# from selenium.webdriver.common.by import By
-#
-# s = Service(ChromeDriverManager().install())
-# chrome = webdriver.Chrome(service=s)
-# chrome.maximize_window()
-# chrome.get('https://www.phptravels.net/')
-# test = chrome.find_element(By.ID,"languages" ).click()
-# sleep(5)
-# chrome.quit()
-
-
-#3 find element by ink text
 from time import sleep
 from selenium import webdriver
 from selenium.webdriver.chrome.service import Service
 from webdriver_manager.chrome import ChromeDriverManager
 from selenium.webdriver.common.by import By
 
-A = Service(ChromeDriverManager().install())
-chrome = webdriver.Chrome(service=A)
+s = Service(ChromeDriverManager().install())
+chrome = webdriver.Chrome(service=s)
 chrome.maximize_window()
-# chrome.get('https://the-internet.herokuapp.com/')
-# chrome.find_element(By.LINK_TEXT,'A/B Testing').click()
+#
+
+#### By Class_name
+#1
+# chrome.get('http://automationpractice.com/index.php')
+#sleep(3)
+# first_name = chrome.find_elements(By.CLASS_NAME, 'sf-with-ul')
+# stocare_elemente = chrome.find_elements(By.CLASS_NAME, 'sf-with-ul')
+# print(len(stocare_elemente))
 # sleep(3)
 # chrome.quit()
 #
-# chrome.get('https://the-internet.herokuapp.com/')
-# chrome.find_element(By.LINK_TEXT,'Context Menu').click()
-# sleep(2)
-# chrome.quit()
-#
-# chrome.get('https://the-internet.herokuapp.com/')
-# chrome.find_element(By.LINK_TEXT,'Typos').click()
-# sleep(3)
-# chrome.quit()
-####
-# chrome.get('https://www.techlistic.com/p/selenium-practice-form.html')
-# sleep(5)
-# chrome.find_element(By.LINK_TEXT,'Automate Amazon like E-Commerce website with Selenium').click()  #nu merge pe acestsite, am incercat mai multe link-uri
-# sleep(5)
-#chrome.quit()
-####
-# chrome.get('https://www.techlistic.com/p/selenium-practice-form.html')    #putem sa scapam de reclame/politica de confidentialitate ca sa ruleze mai departe?
-# sleep(10)
-# chrome.find_element(By.LINK_TEXT,'Automate Google search with Selenium').click()
-# sleep(8)
-#chrome.quit()
-###############
+#2
 # chrome.get('https://jules.app/')
 # sleep(3)
 # chrome.find_elements(By.CLASS_NAME,'MuiInputBase-input')[0].send_keys('marian@gmail.com')
@@ -95,8 +45,90 @@ chrome.maximize_window()
 # test = chrome.find_elements(By.CLASS_NAME,'MuiInputBase-input')[1].send_keys('password')
 # sleep(2)
 # chrome.quit()
+
+#3
+# chrome.get('https://the-internet.herokuapp.com/tinymce')
+# chrome.find_element(By.CLASS_NAME,'tox-mbtn__select-label').click()
+# sleep(3)
+# chrome.quit()
+
+###      By ID
+
+# #1
+
+# chrome.get('https://www.phptravels.net/')
+# test = chrome.find_element(By.ID,"languages" ).click()
+# sleep(5)
+# chrome.quit()
+
+#2
+
+# chrome.get('https://the-internet.herokuapp.com/dropdown')
+# chrome.find_element(By.ID, 'dropdown').click()
+# sleep(3)
+# chrome.quit()
+
+#3
+# chrome.get('https://the-internet.herokuapp.com/dynamic_loading/1')
+# chrome.find_element(By.ID,'start').click()   #  nu da eroare dar nici nu da click....d c?
+# sleep(3)
+# chrome.quit()
+
+#         BY  Link text
+
+
+#1
+# chrome.get('https://the-internet.herokuapp.com/')
+# chrome.find_element(By.LINK_TEXT,'A/B Testing').click()
+# sleep(3)
+# chrome.quit()
+#
+#2
+# chrome.get('https://the-internet.herokuapp.com/')
+# chrome.find_element(By.LINK_TEXT,'Context Menu').click()
+# sleep(2)
+# chrome.quit()
+#
+#3
+# chrome.get('https://the-internet.herokuapp.com/')
+# chrome.find_element(By.LINK_TEXT,'Typos').click()
+# sleep(3)
+# chrome.quit()
+#
+
+#4
+# chrome.get('https://www.techlistic.com/p/selenium-practice-form.html')
+# sleep(5)
+# chrome.find_element(By.LINK_TEXT,'Automate Amazon like E-Commerce website with Selenium').click()  #nu merge pe acestsite, am incercat mai multe link-uri
+# sleep(5)
+#chrome.quit()
+####
+#5
+# chrome.get('https://www.techlistic.com/p/selenium-practice-form.html')    #putem sa scapam de reclame/politica de confidentialitate ca sa ruleze mai departe?
+# sleep(10)
+# chrome.find_element(By.LINK_TEXT,'Automate Google search with Selenium').click()
+# sleep(8)
+#chrome.quit()
 ##
 
+##  BY Partial Link Text
+
+#1
+# chrome.get('https://the-internet.herokuapp.com/dynamic_loading')
+# chrome.find_element(By.PARTIAL_LINK_TEXT, 'Example 1').click()
+# sleep(3)
+# chrome.quit()
+
+#2
+# chrome.get('https://the-internet.herokuapp.com/dynamic_loading')
+# chrome.find_element(By.PARTIAL_LINK_TEXT,'after the fact').click()
+# sleep(3)
+# chrome.quit()
+#3
+# chrome.get('https://jules.app/sign-in')
+# chrome.find_element(By.PARTIAL_LINK_TEXT,'password').click()
+# sleep(3)
+# chrome.quit()
 
 
 
