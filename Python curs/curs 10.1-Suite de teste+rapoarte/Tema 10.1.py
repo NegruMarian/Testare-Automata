587ec35c8125920447158c0a1d3bbe0d26be1990
Marian Negru <Negru Marian Alexandru>
2022-05-03 17:50:48 +0300

    teme

---
+++ Python curs/curs 10.1-Suite de teste+rapoarte/Tema 10.1.py
@@ -0,0 +1,32 @@
+# from time import sleep
+# from selenium import webdriver
+# from selenium.webdriver.chrome.service import Service
+# from webdriver_manager.chrome import ChromeDriverManager
+# from selenium.webdriver.common.by import By
+
+# def setUp(self):
+#     s = Service(ChromeDriverManager().install())
+#     self.chrome = webdriver.Chrome(service=s)
+#     self.chrome.maximize_window()
+#     self.chrome.get('')
+#
+# def tearDown(self):
+#     self.chrome.quit()
+
+
+import unittest
+import HtmlTestRunner # pip install html-testRunner
+
+
+# 1.	Faceti o suita care sa contina testele voastre de la tema 9 + testele de la intalnirea 10
+# (care au clasa). Generati raportul
+# 2.	Scrieti cu sintaxa gherkin toate testele de la tema9 (mai putin 12)
+# 3.	Ganditi voi o clasa de test din paginile sugerate in tema 8 (ce doriti voi, cate functii de test doriti, freestyle ca sa incepeti sa ganditi si singuri niste scenarii de test). Folositi firefox in loc de chrome
+#
+# https://www.phptravels.net/
+# http://automationpractice.com/index.php
+# https://formy-project.herokuapp.com/
+# https://the-internet.herokuapp.com/
+# https://www.techlistic.com/p/selenium-practice-form.html
+#
+# Sau puteti alege voi ce pagina doriti
